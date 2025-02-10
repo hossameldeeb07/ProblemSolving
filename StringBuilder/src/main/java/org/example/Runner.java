d9a86e397a36c2b0801c7646391b6469f4e5d2f5
hossameldeeb07 <94098541+hossameldeeb07@users.noreply.github.com>
2025-02-10 16:50:05 +0200

    Delete StringBuilder directory

--- StringBuilder/src/main/java/org/example/Runner.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Runner {
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        System.out.println(sol.mergeAlternately("hoss","bossakk"));
-    }
-}
